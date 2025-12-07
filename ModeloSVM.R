30fb4e19b6df899cfb4dd38678b17fc97c2ecad7
Cayecaji <cayetanocaji@gmail.com>
2025-12-07 15:47:17 +0100

    Curva aprendizaje en SVM

--- ModeloSVM.R
+++ ModeloSVM.R
@@ -5,10 +5,20 @@
 set.seed(10)
 library(e1071)
 library(caret)
+library(tictoc)
+library(ggplot2)
+library(tidyr)
 
+#Defino un dataframe para imprimir la curva de aprendizaje
+
+resultados_curva <- data.frame(
+  tamano_Muestra = integer(),
+  tiempo_Segundos = numeric(),
+  accuracy = numeric()
+)
 
 #View(train_reducido_df)
-
+tamanos_muestra <- c(1000, 2000, 3000, 4000, 5000)
 
 #Defino el conjunto de entrenamiento y test para aplicar validación cruzada
 ind <- sample(1:nrow(train_reducido_df), 0.8 * nrow(train_reducido_df))
@@ -20,21 +30,13 @@ subsample_index <- sample(1:nrow(train_reducido_df), 1000)
 dtrain_subsample <- dtrain[subsample_index, ]
 dtrain_subsample <- na.omit(dtrain_subsample)
 
-#Me defino otra submuestra para entrenar finalmente el modelo
-subsample_index_2000 <- sample(1:nrow(dtrain), 2000)
-dtrain_subsample_model <- dtrain[subsample_index_2000, ]
-dtrain_subsample_model <- na.omit(dtrain_subsample_model)
-
-#Me defino una submuestra para el conjunto de test
-subsample_index_500 <- sample(1:nrow(dtest), 500)
-dtest_subsample <- dtest[subsample_index_500, ]
-dtest_subsample <- na.omit(dtest_subsample)
-
 
 #Definimos los valores de coste y gamma a probar en el tune
 coste_valores <- 10^(-1:2)    
 gamma_valores <- 10^(-3:0)   
 par_grid <- list(cost = coste_valores, gamma = gamma_valores)
+
+
 
 #Iniciamos el tune
 cat("Iniciando la búsqueda de hiperparámetros...\n")
@@ -58,27 +60,53 @@ cat("Mejor Costo (C):", mejor_costo, "\n")
 cat("Mejor Gamma (γ):", mejor_gamma, "\n")
 
 #Una vez tenemos los mejores hiperparámetros, entrenamos el svm final
-svmDigit <- svm(label ~ ., data=dtrain_subsample_model, kernel = "radial", cost = mejor_costo, gamma = mejor_gamma)
+for(i in 1:length(tamanos_muestra)){
+  #Me defino otra submuestra para entrenar finalmente el modelo
+  subsample_index <- sample(1:nrow(dtrain), tamanos_muestra[i])
+  dtrain_subsample_model <- dtrain[subsample_index, ]
+  dtrain_subsample_model <- na.omit(dtrain_subsample_model)
+  
+  
+  tic("--------Entrenamiento del modelo SVM--------")
+  svmDigit <- svm(label ~ ., data=dtrain_subsample_model, kernel = "radial", cost = mejor_costo, gamma = mejor_gamma)
+  tiempo_entrenamiento <- toc()
+  tiempo_en_segundos <- tiempo_entrenamiento$toc - tiempo_entrenamiento$tic
+ 
+   #Calculamos el accuracy
+  pred <- predict(svmDigit, dtest, type = "class")
+  matrizconfusion <- table(pred, dtest$label)
+  accuracy <- sum(diag(matrizconfusion)) / sum(matrizconfusion)
+  
+  cat("El accuracy obtenido por el modelo SVM con dataset DigitRecognition es del:", accuracy*100,"%")
+  cat("Tiempo empleado en entrenar el modelo:", tiempo_en_segundos ,"s \n")
+  
+  resultados_curva[nrow(resultados_curva) + 1, ] <- c(
+    nrow(dtrain_subsample_model),
+    tiempo_en_segundos,
+    accuracy
+  )
+  
+}
 
 
-#Calculo el accuracy del modelo 
-pred <- predict(svmDigit,dtest_subsample,type="class")
-matrizconfusion <- table(pred, dtest_subsample$label)
+print(resultados_curva)
 
-#Mostramos la matriz de confusión
-print(matrizconfusion)
+#Mostramos la curva de aprendizaje
+resultados_long <- resultados_curva %>%
+  pivot_longer(
+    cols = c("tiempo_Segundos", "accuracy"),
+    names_to = "Parametros",
+    values_to = "Valor"
+  )
 
-#Calculamos el accuracy
-accuracy<-sum(diag(matrizconfusion)/sum(matrizconfusion))
-
-cat("El accuracy obtenido por el modelo SVM con dataset DigitRecognition es del:", accuracy*100,"%")
-
-
-
-
-
-
-
-
-
-
+ggplot(resultados_long, aes(x = tamano_Muestra, y = Valor, color = Parametros)) +
+  geom_line(linewidth = 1) +
+  geom_point(size = 3) +
+  labs(
+    title = "Curva de Aprendizaje para el modelo SVM",
+    x = "Tamaño del Conjunto de Entrenamiento (Muestras)",
+    y = "",
+    color = "Parámetros utilizados"
+  ) +
+  scale_color_manual(values = c("accuracy" = "blue", "tiempo_Segundos" = "red")) +
+  theme_minimal()
