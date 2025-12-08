5f442df4a1f5dd528cd06dfabde03bfd6fb238a6
Cayecaji <cayetanocaji@gmail.com>
2025-12-08 20:26:53 +0100

    Cambiado el perceptrón para que sea igual que el resto de modelos
    Añadido el tune y la curva de aprendizaje al perceptrón. Eliminado el perceptron del main y añadido a un archivo a parte.

--- DigitRecognition.R
+++ DigitRecognition.R
@@ -1,10 +1,9 @@
 library(tidyverse)
 suppressMessages(library(caret))
-library(nnet)  # para el perceptrón multicapa (MLP, perceptrón multicapa)
 
 
-train <- read.table("C:/Users/Jaime/Documents/Uni/4/TLP/DigitRecognition/train.csv",header=T, sep=",")
-test <- read.table("C:/Users/Jaime/Documents/Uni/4/TLP/DigitRecognition/test.csv",header=T, sep=",")
+train <- read.table("C:/Users/TT CUSTOM/Desktop/DigitRecognition/train.csv",header=T, sep=",")
+test <- read.table("C:/Users/TT CUSTOM/Desktop/DigitRecognition/test.csv",header=T, sep=",")
 
 dim(train)
 dim(test)
@@ -58,24 +57,18 @@ mostrar_digito(trainlst$x, 7, tiene_label = FALSE)
 #)
 
 
-# --- PASO 1: Crear la Matriz de Reducción ---
+# -------- Convolución de la imagen  -------- #
 
-# 1. Matriz W: Promedia pares vecinos (1D)
 # Crea una matriz que transforma vector de 28 a 14 promediando cada 2.
 W <- diag(14)[rep(1:14, each = 2), ] / 2
 
-# 2. Matriz Pool2x2: Expande la transformación a 2D
 # Usa el producto Kronecker para aplicar W tanto a filas como a columnas
 Pool2x2 <- W %x% W 
 
 # Asignar nombres a las nuevas columnas (pixel1...pixel196)
 colnames(Pool2x2) <- paste0("p", 1:ncol(Pool2x2))
 
-
-# --- PASO 2: Aplicar la reducción ---
-
 # Aplicamos la transformación a la matriz de imágenes 'x' que creamos antes
-# Asegúrate de usar la matriz numérica 'trainlst$x', no el dataframe original
 X_reducida <- trainlst$x %*% Pool2x2 
 X_reducida <- X_reducida /255
 train_reducido_df <- as.data.frame(X_reducida)
@@ -84,16 +77,15 @@ train_reducido_df$label <- trainlst$y
 
 # Verificar la reducción
 dim(X_reducida) 
-# Debería salir: [42000, 196]. ¡Has bajado de 784 a 196 columnas!
 
-## Capturita
+# Mostramos el digito con la imagen original junto con la convolución realizada
 par(mfrow=c(1,2))
 mostrar_digito(trainlst$x, 7, tiene_label = FALSE)
 mostrar_digito(X_reducida, 7, nrow = 14, ncol = 14, tiene_label = FALSE)
 
 
 
-##### --- PARTE DE JAIME: ELIMINACIÓN DE FILAS Y COLUMNAS --- #####
+# --------  Eliminación de filas y columnas --------- #
 
 # Calculamos la media de intensidad de cada píxel en todo el train
 pixel_means <- colMeans(trainlst$x)
@@ -104,7 +96,6 @@ row_means <- rowMeans(mean_mat)
 col_means <- colMeans(mean_mat)
 
 # Nos quedamos con las filas y columnas que no son completamente negras
-# Si quieres ser más agresivo, cambia el 0 por, por ejemplo, 1 o 2
 umbral <- 5
 
 rows_to_keep <- which(row_means > umbral)
@@ -116,7 +107,7 @@ pixel_mat   <- matrix(pixel_names, nrow = 28, ncol = 28, byrow = TRUE)
 pixels_keep <- as.vector(pixel_mat[rows_to_keep, cols_to_keep])
 
 length(pixel_names)  # 784 originales
-length(pixels_keep)  # píxeles tras eliminar filas/columnas
+length(pixels_keep)  # píxeles tras eliminar filas/columnas (418)
 
 # Construimos una nueva matriz de entrenamiento solo con esos píxeles
 X_border <- trainlst$x[, pixels_keep]
@@ -132,45 +123,6 @@ new_ncol <- length(cols_to_keep)
 # Visualizar original vs recortado (eliminación filas/columnas)
 par(mfrow = c(1,2))
 mostrar_digito(trainlst$x, 7, nrow = 28, ncol = 28, tiene_label = FALSE)
-mostrar_digito(X_border,     7, nrow = new_nrow, ncol = new_ncol, tiene_label = FALSE)
+mostrar_digito(X_border, 7, nrow = new_nrow, ncol = new_ncol, tiene_label = FALSE)
 
 
-
-##### --- PARTE DE JAIME: PERCEPTRÓN MULTICAPA (MLP) --- #####
-
-# Normalizamos los píxeles a [0,1]
-mlp_df <- train_border_df
-mlp_df[, -ncol(mlp_df)] <- mlp_df[, -ncol(mlp_df)] / 255
-
-# Convertimos la etiqueta a factor para clasificación multiclase
-mlp_df$label <- as.factor(mlp_df$label)
-
-# Partimos en train / validación (80% / 20%)
-set.seed(123)
-idx_train <- createDataPartition(mlp_df$label, p = 0.8, list = FALSE)
-d_train   <- mlp_df[idx_train, ]
-d_valid   <- mlp_df[-idx_train, ]
-
-# Perceptrón multicapa con una capa oculta (size neuronas ocultas)
-mlp_model <- nnet(
-  label ~ .,
-  data   = d_train,
-  size   = 50,       # número de neuronas en la capa oculta
-  decay  = 5e-4,     # regularización
-  maxit  = 50,      # iteraciones máximas
-  MaxNWts = 100000,  # por si hay muchos pesos
-  trace  = TRUE
-)
-
-# Predicciones en el conjunto de validación
-pred_valid <- predict(mlp_model, d_valid[, -ncol(d_valid)], type = "class")
-
-# Aseguramos que pred_valid sea factor con los mismos niveles que las etiquetas reales
-pred_valid <- factor(pred_valid, levels = levels(d_valid$label))
-
-# Matriz de confusión y accuracy del MLP
-cm_mlp <- confusionMatrix(pred_valid, d_valid$label)
-cm_mlp
-
-accuracy_mlp <- as.numeric(cm_mlp$overall["Accuracy"])
-accuracy_mlp
